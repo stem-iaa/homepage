b2b381e5518548e07296fa87eafab2a0b4afdb16
Sam Clark <sam.clark1142@gmail.com>
2019-07-23 15:17:06 -0400

    before polymorphic model

--- Model/User.py
+++
@@ -1,8 +0,0 @@
-from app import db
-
-
-class User(db.Model):
-    username = db.Column(db.String(64), index=True, unique=True, primary_key=True)
-    password_hash = db.Column(db.String(128))
-    user_type = db.Column(db.String(64))
-
--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import flask_login
 import json
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from werkzeug.security import generate_password_hash, check_password_hash
 
 config = json.load(open("config.json", "r"))
 
@@ -17,6 +18,8 @@ migrate = Migrate(app, db)
 login_manager = flask_login.LoginManager()
 login_manager.init_app(app)
 
+import models
+
 
 class User(flask_login.UserMixin):
     pass
@@ -28,48 +31,56 @@ users = json.load(open("users.json", "r"))
 @login_manager.user_loader
 def user_loader(username):
     if username not in users:
-        return
+        return None
 
     user = User()
     user.id = username
     return user
 
 
-@app.route('/')
+@app.route("/")
 def hello_world():
     return render_template("index.html")
 
 
-@app.route('/login', methods=['GET', 'POST'])
+@app.route("/login", methods=["GET", "POST"])
 def login():
-    if request.method == 'GET':
+    if request.method == "GET":
         return render_template("login.html")
-    elif request.method == 'POST':
-        username = request.form.get('username')
-        password = request.form.get('password')
+    elif request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
 
         if not username or not password:
             return "Bad login"
 
-        if password == users[username]['password']:
+        if password == users[username]["password"]:
             user = User()
             user.id = username
             flask_login.login_user(user)
-            return redirect(url_for('protected'))
+            return redirect(url_for("protected"))
 
     return "Bad login"
 
 
-@app.route('/protected')
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "GET":
+        return render_template("register.html")
+    elif request.method == "POST":
+        pass
+
+
+@app.route("/protected")
 @flask_login.login_required
 def protected():
     return "protected"
 
 
-@app.route('/test')
+@app.route("/test")
 def test():
     return users
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run()
