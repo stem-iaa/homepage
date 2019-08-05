373493237b775a40bb0ca843c30dd9fca34b7370
Sam Clark <sam.clark1142@gmail.com>
2019-08-05 14:27:55 -0400

    db migrations

---
+++ util/create_admin.py
@@ -0,0 +1,6 @@
+from app import db, models, generate_password_hash
+
+admin = models.Instructor(username="sam", password_hash=generate_password_hash("sam"))
+
+db.session.add(admin)
+db.session.commit()
