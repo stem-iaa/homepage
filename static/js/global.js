9f908c2272ab8ed6ff35f3a32b3cc80bf875a3e1
Sam Clark <sam.clark1142@gmail.com>
2019-08-15 17:04:38 -0400

    switch to jquery for smooth scroll

--- static/js/global.js
+++ static/js/global.js
@@ -8,3 +8,15 @@ function getCurrentUser(callback) {
         }
     });
 }
+
+$('a[href*="#"]').on('click', function (e) {
+    e.preventDefault();
+
+    $('html, body').animate(
+        {
+            scrollTop: $($(this).attr('href')).offset().top,
+        },
+        400,
+        'swing'
+    );
+});
