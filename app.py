02d5675efc1beba5bc68d39e11872163da7ff16b
Sam Clark <sam.clark1142@gmail.com>
2019-08-15 19:14:16 -0400

    protected static files folder

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, abort
 import flask_login
 import json
 from flask_sqlalchemy import SQLAlchemy
@@ -9,7 +9,24 @@ from flask_migrate import Migrate
 
 config = json.load(open("config.json", "r"))
 
-app = Flask(__name__)
+
+# https://stackoverflow.com/questions/12118355/secure-static-files-with-flask
+class SecuredStaticFlask(Flask):
+    def send_static_file(self, filename):
+        root_dir = filename.split("/")[0]
+        if root_dir != "protected":
+            return super(SecuredStaticFlask, self).send_static_file(filename)
+
+        current_user = flask_login.current_user
+        if current_user.is_anonymous:
+            abort(403)
+        if current_user.discriminator == "student":
+            abort(403)
+
+        return super(SecuredStaticFlask, self).send_static_file(filename)
+
+
+app = SecuredStaticFlask(__name__)
 app.secret_key = config["secret"]
 app.config["SQLALCHEMY_DATABASE_URI"] = config["SQLALCHEMY_DATABASE_URI"]
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
