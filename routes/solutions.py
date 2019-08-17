6a95daf815165bf1f5acbe6f046e0eb3f1878a13
Sam Clark <sam.clark1142@gmail.com>
2019-08-16 20:52:28 -0400

    solutions page init

---
+++ routes/solutions.py
@@ -0,0 +1,19 @@
+from app import app
+from app import db
+from flask import Flask, render_template, request, redirect, url_for, abort
+import flask_login
+from werkzeug.security import generate_password_hash, check_password_hash
+import model
+import json
+from sqlalchemy import or_
+
+
+@app.route("/solutions", methods=["GET"])
+@flask_login.login_required
+def solutions():
+    current_user = flask_login.current_user
+    if current_user.discriminator == "student":
+        return abort(403)
+
+    return render_template("solutions.html",
+                           user=current_user)
